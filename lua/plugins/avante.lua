ce83e07008e53703f3fc4f87175291069f7cb212
Florian Chardin <othalla.lf@gmail.com>
2025-12-04 10:16:53 +0100

    Add gemini 2.5 flash provider to Avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,8 +3,8 @@ return {
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   -- ⚠️ must add this setting! ! !
   build = vim.fn.has("win32") ~= 0
-      and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      or "make",
+  and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+  or "make",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   ---@module 'avante'
@@ -14,16 +14,24 @@ return {
     -- this file can contain specific instructions for your project
     instructions_file = "avante.md",
     -- for example
-		provider = "copilot",
+    provider = "copilot",
     providers = {
+      gemini = {
+        endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+        model = "gemini-2.5-flash",
+        timeout = 30000,
+        temperature = 0,
+        max_tokens = 4096,
+      },
+
       claude = {
         endpoint = "https://api.anthropic.com",
         model = "claude-sonnet-4-20250514",
         timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
       },
     },
   },
